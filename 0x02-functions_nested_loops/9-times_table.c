9c8df7463f2555320c53eaf3bcc921a5900f9208
aquarian-aya <ayajakuja@gmail.com>
2023-03-05 05:52:59 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 			if (j == 0)
 			{
 				_putchar(k + '0');
-				}
+			}
 
 			if (k < 10 && j != 0)
 			{
@@ -32,6 +32,6 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 			}
 		}
-		_putchar('\n');
+			_putchar('\n');
 	}
 }
